a349958af93f309df31d79bd2ebf1b8b60156ddf
Abhishek kushwaha <kushwahaabhishesk2001@gmail.com>
2026-03-21 23:57:03 +0530

    commit d

--- src/pages/Allticket.jsx
+++ src/pages/Allticket.jsx
@@ -114,15 +114,10 @@ function Allticket() {
 
             {/* हमेशा ये button रखो (best practice) */}
             <button
-              onClick={() =>
-                window.open(
-                  `https://docs.google.com/gview?url=${ticket.attachment}&embedded=true`,
-                  "_blank"
-                )
-              }
+              onClick={() => window.open(ticket.attachment, "_blank")}
               className="mt-3 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
             >
-              Open Preview
+              Open Full
             </button>
           </>
         ) : (
